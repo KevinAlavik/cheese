7cc2f4ba465a1ef93a6cee8ad7a452772f7629d9
puffer <kevin@alavik.se>
2024-02-28 14:51:23 +0000

    did some things

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # cheese
-A (stupid) bootloader and OS for x86_64
+A (stupid) bootloader and OS for x86
 
 ## Why?
 You might ask why im making another bootloader when there already exist tons of better ones. Well its for me to learn! And might as well put it here :^)
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,13 +1,7 @@
-// This is just a placeholder - Kevin
-// Nuh uh, not anymore. - KapPetrov
-
-#include "graphics.h"
+#include "lib/serial.h"
 
 void main()
 {
-    line(10, 10, 70, 0x0f);
-    line_d(10, 10, 70, 0x0f);
-    line_di(200, 20, 70, 0x0f);
-    line_v(10, 10, 70, 0x0f);
-    return;                // Return from function.
+    outb8(QEMU_SERIAL_PORT, 'A');
+    return;
 }
